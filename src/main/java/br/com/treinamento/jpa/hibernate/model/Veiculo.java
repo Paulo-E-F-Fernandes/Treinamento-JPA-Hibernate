b47f4ea721e25f25d84304ddf37619cb155f6d3b
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-10 22:36:30 -0300

    Adição dos métodos hashCode() e equals() do Veículo

--- src/main/java/br/com/treinamento/jpa/hibernate/model/Veiculo.java
+++ src/main/java/br/com/treinamento/jpa/hibernate/model/Veiculo.java
@@ -59,4 +59,37 @@ public class Veiculo {
 		this.valor = valor;
 	}
 
+	/* 
+	 * Os métodos hashCode() e equals() são necessários para que os objetos persistentes
+	 *  sejam diferenciados um dos outros.
+	 *  
+	 * Com estes métodos, o Hibernate consegue comparar objetos para descobrir se são os
+	 *  mesmos.
+	 */
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((codigo == null) ? 0 : codigo.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Veiculo other = (Veiculo) obj;
+		if (codigo == null) {
+			if (other.codigo != null)
+				return false;
+		} else if (!codigo.equals(other.codigo))
+			return false;
+		return true;
+	}
+	
 }

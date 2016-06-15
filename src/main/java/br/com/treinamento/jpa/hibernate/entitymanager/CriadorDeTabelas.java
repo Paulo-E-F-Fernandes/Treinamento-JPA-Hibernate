3a97cec3c791d1326c549e5ab5ce0ba4824c22e6
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-15 20:25:15 -0300

    Criação do EntityManagerFactory

---
+++ src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/CriadorDeTabelas.java
@@ -0,0 +1,13 @@
+package br.com.treinamento.jpa.hibernate.entitymanager;
+
+import javax.persistence.Persistence;
+
+public class CriadorDeTabelas {
+
+	public static void main(String[] args) {
+		/* O parâmetro do método "createEntityManagerFactory" é o "name" do
+		   "persistence-unit" do persistence.xml */
+		Persistence.createEntityManagerFactory("TreinamentoJPAHibernatePU");
+	}
+
+}

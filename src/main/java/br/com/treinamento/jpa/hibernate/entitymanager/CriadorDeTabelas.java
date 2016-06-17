45b9fe7ac21ef80d1f390348372bb1923299de0f
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-17 20:10:00 -0300

    Correção nos comentários

--- src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/CriadorDeTabelas.java
+++ src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/CriadorDeTabelas.java
@@ -5,8 +5,7 @@ import javax.persistence.Persistence;
 public class CriadorDeTabelas {
 
 	public static void main(String[] args) {
-		/* O parâmetro do método "createEntityManagerFactory" é o "name" do
-		   "persistence-unit" do persistence.xml */
+		/* O parâmetro do método "createEntityManagerFactory" é o "name" do "persistence-unit" do persistence.xml */
 		Persistence.createEntityManagerFactory("TreinamentoJPAHibernatePU");
 	}
 

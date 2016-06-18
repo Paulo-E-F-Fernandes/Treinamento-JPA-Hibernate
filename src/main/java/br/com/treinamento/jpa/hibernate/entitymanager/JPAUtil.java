455729c38827753b4ba661d9f4e0639f11b9089f
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-18 18:27:18 -0300

    Criação do JPAUtil, o EntityManagerFactory da aplicação

---
+++ src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/JPAUtil.java
@@ -0,0 +1,34 @@
+package br.com.treinamento.jpa.hibernate.entitymanager;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+/*
+Como os sistemas que usam JPA precisam de apenas uma instância do EntityManagerFactory, esta pode ser
+criada durante a inicialização da aplicação e depois qualquer parte do sistema que precisar de um
+EntityManager pode solicitar a está instância.
+
+Podemos criar um bloco estático para inicializar o EntityManagerFactory e como isso ocorrerá apenas
+no carregamento da classe, sempre teremos uma instância da Factory, agora quando precisarmos de uma 
+EntityManager, usamos o seguinte código:
+
+EntityManager manager = JpaUtil.getEntityManager();
+*/
+public class JPAUtil {
+	
+	private static EntityManagerFactory factory;
+	
+	static {
+		factory = Persistence.createEntityManagerFactory("TreinamentoJPAHibernatePU");
+	}
+	
+	public static EntityManager getEntityManager() {
+		return factory.createEntityManager();
+	}
+	
+	public static void close() {
+		factory.close();
+	}
+
+}

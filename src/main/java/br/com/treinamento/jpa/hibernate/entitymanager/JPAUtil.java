19b8e25b6c27a565c15e2caba38a874d15ebc7b5
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-18 19:18:23 -0300

    Criação do EntityManagerFactory estático da aplicação

--- src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/JPAUtil.java
+++ src/main/java/br/com/treinamento/jpa/hibernate/entitymanager/JPAUtil.java
@@ -5,16 +5,16 @@ import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 /*
-Como os sistemas que usam JPA precisam de apenas uma instância do EntityManagerFactory, esta pode ser
-criada durante a inicialização da aplicação e depois qualquer parte do sistema que precisar de um
-EntityManager pode solicitar a está instância.
-
-Podemos criar um bloco estático para inicializar o EntityManagerFactory e como isso ocorrerá apenas
-no carregamento da classe, sempre teremos uma instância da Factory, agora quando precisarmos de uma 
-EntityManager, usamos o seguinte código:
-
-EntityManager manager = JpaUtil.getEntityManager();
-*/
+ * Como os sistemas que usam JPA precisam de apenas uma instância do EntityManagerFactory, esta pode
+ *  ser criada durante a inicialização da aplicação e depois qualquer parte do sistema que precisar 
+ *  de um EntityManager pode solicitar a está instância.
+ * 
+ * Podemos criar um bloco estático para inicializar o EntityManagerFactory e como isso ocorrerá 
+ *  apenas no carregamento da classe, sempre teremos uma instância da Factory, agora quando 
+ *  precisarmos de uma EntityManager, usamos o seguinte código:
+ * 
+ * EntityManager manager = JpaUtil.getEntityManager();
+ */
 public class JPAUtil {
 	
 	private static EntityManagerFactory factory;

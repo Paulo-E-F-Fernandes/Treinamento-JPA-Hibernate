4f6a78a20697b23160ef9ee989107c943fbb491e
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-19 11:53:07 -0300

    Adição de comentários para explicação do funcionamento do método find()

--- src/main/java/br/com/treinamento/jpa/hibernate/repository/BuscadorVeiculo1.java
+++ src/main/java/br/com/treinamento/jpa/hibernate/repository/BuscadorVeiculo1.java
@@ -10,8 +10,13 @@ public class BuscadorVeiculo1 {
 	public static void main(String[] args) {
 		EntityManager manager = JPAUtil.getEntityManager();
 		
+		/* Buscando um objeto pelo identificador usando o método find(Class<T>, Object).
+		 *
+		 * O método find(Class<T>, Object) busca o objeto imediatamente no banco de dados.
+		 */
 		Veiculo veiculo = manager.find(Veiculo.class, 1L);
 		
+		System.out.println("--- Buscou veículo. Será que já executou o SELECT? ---");
 		System.out.println("Veículo de código " + veiculo.getCodigo() + " é um " + veiculo.getModelo());
 		
 		manager.close();

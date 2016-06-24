7db5d9ca449647f470da2c0ad4bdeee57d62e76c
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2016-06-24 15:34:56 -0300

    Teste de cache de primeiro nível, entity manager único

---
+++ src/main/java/br/com/treinamento/jpa/hibernate/TesteCachePrimeiroNivel.java
@@ -0,0 +1,42 @@
+package br.com.treinamento.jpa.hibernate;
+
+import javax.persistence.EntityManager;
+
+import br.com.treinamento.jpa.hibernate.entitymanager.JPAUtil;
+import br.com.treinamento.jpa.hibernate.model.Veiculo;
+
+public class TesteCachePrimeiroNivel {
+
+	/* Uma mesma entidade pode ser representada por diferentes objetos na memória,
+	 * desde que seja em diferentes instâncias de EntityManagers. 
+	 */
+	public static void main(String[] args) {
+		System.out.println("***testarMesmoEntityManager***");
+		testarMesmoEntityManager();
+	}
+
+	/* 
+	 * Em uma única instância de EntityManager, apenas um objeto que representa determinada
+	 * entidade (com o mesmo identificador) pode ser gerenciada.
+	 */
+	private static void testarMesmoEntityManager() {
+		EntityManager manager = JPAUtil.getEntityManager();
+		
+		Veiculo veiculo1 = manager.find(Veiculo.class, 2L);
+		System.out.println("Buscou o veículo pela primeira vez...");
+		
+		Veiculo veiculo2 = manager.find(Veiculo.class, 2L);
+		System.out.println("Buscou o veículo pela segunda vez...");
+		
+		System.out.println("Mesmo veículo? " + (veiculo1 == veiculo2));
+		
+		manager.close();
+		JPAUtil.close();
+	}
+
+	
+	
+	
+	
+	
+}
